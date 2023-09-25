9406dcffa1035472d2ede244f7759f51a90759c1
Jairosz <Jairosg03@gmail.com>
2023-09-25 22:30:39 +0200

    Create Sanchez_Gavilan_U5_Ejs.java

---
+++ Libre_Configuración/Sanchez_Gavilan_U5_Ejs.java
@@ -0,0 +1,23 @@
+/* 
+1.- Conteste con verdadero o falso a cada una de las siguientes proposiciones; en caso de ser falso,
+explique por qué.
+a) Cuando los objetos String se comparan utilizando ==, el resultado es true si los objetos
+String contiene los mismos valores.
+    Falso, compara si son el mismo objeto, es decir si ocupan el mismo SLOT DE MEMORIA, pero para comparar los valores se usa EQUALS.
+b) Un objeto String puede modificarse una vez creado.
+    Verdadero
+2.- Para cada uno de los siguientes enunciados, escriba una instrucción que realice la tarea indicada:
+a) Comparar la cadena en s1 con la cadena en s2 para ver si su contenido es igual.
+    boolean sonIguales = s1.equals(s2);
+b) Anexar la cadena s2 a la cadena s1, utilizando +=.
+    s1 += s2;
+c) Determinar la longitud de la cadena en s1.
+    int longitud = s1.length();
+ */
+
+
+public class Sanchez_Gavilan_U5_Ejs {
+    public static void main(String[] args) {
+        
+    }
+}
