70188c6039ad399f40372cd9fa410da7224ff6a4
Jairosz <Jairosg03@gmail.com>
2023-10-03 22:39:27 +0200

    Create Sanchez_Gavilan_Arrays15.java

---
+++ Libre_Configuración/Practica/Sanchez_Gavilan_Arrays15.java
@@ -0,0 +1,45 @@
+package Practica;
+import java.util.*;
+public class Sanchez_Gavilan_Arrays15 {
+    public static void main(String[] args) {
+        boolean avionAsientos [] = new boolean [10];
+        int opcionMenu = 0;
+        boolean salida = false;
+        String nombre = "";
+        String apellidos = "";
+        String dni = "";
+        Scanner teclado = new Scanner (System.in);
+        do{
+            System.out.println("AEROLÍNEAS JAIRING");
+            System.out.println("1. ## COMPRAR BILLETE PRIMERA CLASE ");
+            System.out.println("2. ## COMPRAR BILLETE ECONÓMICO ");
+            System.out.println("3. ## MOSTRAR DISPONIBILIDAD ");
+            System.out.println("4. ## SALIR ");
+
+            switch(opcionMenu){
+                case 1: // billete primera clase
+                    for(int i = 0; i >= avionAsientos.length; i++){
+
+                    }
+                break;
+                case 2: // billete economico 
+                break;
+                case 3: // mostrar disponibilidad
+                break;
+                case 4: // salida
+                break;
+                default:
+                System.out.println("Valor incorrecto");
+
+
+
+            }
+
+    
+    
+    
+    
+    
+        }while(salida!=false);
+    }
+}
