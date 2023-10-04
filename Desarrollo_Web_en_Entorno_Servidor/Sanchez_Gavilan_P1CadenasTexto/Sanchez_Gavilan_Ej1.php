06172bd07fdccd4aa350a4570d7a0cf6a7f910c0
Jairosz <Jairosg03@gmail.com>
2023-10-04 07:05:48 +0200

    Create Sanchez_Gavilan_Ej1.php

---
+++ Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+  <?php 
+    $palabra1 = "roma";
+    $palabra2 = "geroma";
+    $palabra13ultimas = substr($palabra1,-3);
+    $palabra23ultimas = substr($palabra2,-3);
+    $palabra12ultimas = substr($palabra1,-2);
+    $palabra22ultimas = substr($palabra2,-2);
+
+    echo ("Las palabras utilizadas son:")
+    echo ("roma y geroma")
+
+
+    if($palabra13ultimas === $palabra23ultimas){
+        echo ("Estas palabras si riman")
+    }else if($palabra12ultimas === $palabra22ultimas)
+        echo ("Estas palabras riman un poco")
+    }else{
+        echo ("Estas no riman")
+    }
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
