06d200ae06412fbf3a4f6058890d1b208563a535
Jairosz <Jairosg03@gmail.com>
2023-10-08 22:11:34 +0200

    a
    a

---
+++ Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <? 
+    $cadena = hola soy jairo
+
+    $palabras = explode(" ", $cadena)
+
+    for($i = 0; $palabras == 2; $i ++;){
+
+        echo ($palabras[i]) . " ";
+    }
+    
+    
+    ?>
+</body>
+</html>
