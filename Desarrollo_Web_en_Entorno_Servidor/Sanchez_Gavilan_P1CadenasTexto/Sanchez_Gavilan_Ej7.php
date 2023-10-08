7f43fc864482080bb779277b0a41d9dcbeb9a011
Jairosz <Jairosg03@gmail.com>
2023-10-08 22:32:22 +0200

    a
    a

--- Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej7.php
+++ Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej7.php
@@ -7,11 +7,18 @@
 </head>
 <body>
     <?php 
-         $frase = "Mi carro me lo robaron, estando de romería. Mi carro me lo robaron, de noche cuando dormía. ¿Dónde estará mi carro? ¿dónde estará mi carro?"
+        $frase = "Mi carro me lo robaron, estando de romería. Mi carro me lo robaron, de noche cuando dormía. ¿Dónde estará mi carro? ¿dónde estará mi carro?";
 
+        $fraseRepetida = "";
         for ($i = 0; $i < strlen($frase); $i++) {
-            echo str_repeat($frase[$i], 2);
+            if (ctype_alpha($frase[$i])) {
+                $fraseRepetida .= str_repeat($frase[$i], 2);
+            } else {
+                $fraseRepetida .= $frase[$i];
+            }
         }
+        
+        echo $fraseRepetida;
     ?>
 </body>
 </html>
