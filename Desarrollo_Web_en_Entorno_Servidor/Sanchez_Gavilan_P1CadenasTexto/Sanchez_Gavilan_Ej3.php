eaee242e387565395034f40c0ba51b05356f9429
Jairosz <Jairosg03@gmail.com>
2023-10-08 21:25:36 +0200

    a
    a

--- Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej3.php
+++ Desarrollo_Web_en_Entorno_Servidor/Sanchez_Gavilan_P1CadenasTexto/Sanchez_Gavilan_Ej3.php
@@ -7,17 +7,14 @@
 </head>
 <body>
     <?php
-    // Nombre de usuario que deseas validar
     $nombreUsuario = "jairo_sg03";
 
-    // Comprobación de longitud permitida
     $longitudMinima = 3;
     $longitudMaxima = 20;
 
     if (strlen($nombreUsuario) < $longitudMinima || strlen($nombreUsuario) > $longitudMaxima) {
         echo "La longitud del nombre de usuario debe estar entre $longitudMinima y $longitudMaxima caracteres.";
     } else {
-        // Comprobación de caracteres válidos
         if (preg_match('/^[a-zA-Z0-9-_]+$/', $nombreUsuario)) {
             echo "El nombre de usuario es válido: $nombreUsuario";
         } else {
