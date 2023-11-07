e4805dd81706cc7d179acd9d7fb72c69e50cff24
Jairosz <Jairosg03@gmail.com>
2023-11-07 13:19:20 +0100

    Update Ejercicio1Examen.php

--- Ejercicio1Examen.php
+++ Ejercicio1Examen.php
@@ -2,36 +2,41 @@
     $modo = 3;
     switch ($modo) {
         case 1: // COMPARAR CADENAS $MOD0 = 1 
-            $palabra1 ="";
-            $palabra2 ="";
-            if (substr($palabra) === substr($palabra2)) {
-            echo 'Son iguales'; 
-                } else {
-                 echo 'Las palabras no son iguales'; 
-             }
-            break;
-        case 2: // cOMPARAR CADENAS CASE SENSITIVE 
+            $cadena1 = "Hola Mundo";
+            $cadena2 = "hola mundo";
+            
+            if ($cadena1 === $cadena2) {
+                echo "Las cadenas son iguales (sensible a mayúsculas y minúsculas)";
+            } else {
+                echo "Las cadenas son diferentes";
+            }
+            
+        case 2: // cOMPARAR CADENAS CASE SENSITIVE  $MOD0 = 2 
         if (strcasecmp($palabra1, $palabra2) === 0) {
                 echo "";
         } else {
                 echo "";
         }
             break;
-        case 3: // comparacion natural strnatcmp(string $str1, string $str2): int
-            $str1 = "red";
-            $str2 = "redent";
-            $test=strncasecmp($str1, $str2 , 3); 
-            if ($str1==$str2){
-            echo "$test"; 
-            }else{ 
-            echo "$test" ;
-            }
+        case 3: // comparacion natural 
+        
             break;
-        case 4: // strncasecmp(string $str1, string $str2, int $len): int
+        case 4: // strncmp(string $str1, string $str2, int $len): int
+            $cadena3 = "Jairo";
+            $cadena4 = "Jairo como estás";
+            
+            $n = 5; 
+            
+            if (strncmp($cadena1, $cadena2, $n) === 0) {
+                echo "Las primeras $n letras de las cadenas son iguales";
+            } else {
+                echo "Las cadenas difieren en las primeras $n letras";
+            }
+            
             break;
         default:
     }
-    
+
 
 
 
