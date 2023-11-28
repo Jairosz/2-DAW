e36d4e2b4d867e7aed9b25c78f80d714ba8b45fc
Jairosz <Jairosg03@gmail.com>
2023-11-28 13:34:11 +0100

    A
    A

--- Desarrollo_Web_en_Entorno_Servidor/repositorioexamen.php
+++ Desarrollo_Web_en_Entorno_Servidor/repositorioexamen.php
@@ -39,9 +39,27 @@ function dibujarArbolNavidad($altura) {
         echo "\n";
     }
 }
+//Ejercicio 4 Realizar una funcion que recibe como parametro una cadena y un valor numerico. La funcion debe devolver una cadena igual que la que se paso por parametro excepto en que se ha eliminado la palabra situada en la posicion que se ha pasado por parametro. Se debe hacer utilizando arrays. Si se especifica un numero de palara que no existe debe escribir un mensaje de error
+function eliminarPalabraEnPosicion($cadena, $posicion) {
+    $palabras = explode(" ", $cadena); // Convertir la cadena en un array de palabras
+    
+    if ($posicion < 1 || $posicion > count($palabras)) {
+        return "Error: La posición especificada no existe.";
+    }
 
-// Probando la función con altura 4
-dibujarArbolNavidad(4);
+    $posicion--; 
+    array_splice($palabras, $posicion, 1); 
+    
+    $nuevaCadena = implode(" ", $palabras); 
+    
+    return $nuevaCadena;
+}
+
+$cadena = "ME GUSTA MUCHO PHP Y JAVASCRIPT";
+$posicionAEliminar = 4;
+
+$resultado = eliminarPalabraEnPosicion($cadena, $posicionAEliminar);
+echo $resultado;
 
 
 //funciones
