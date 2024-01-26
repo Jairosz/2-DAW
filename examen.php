7ea9c905c2c702d5511dc897ee49bd730dc7d398
Jairosz <Jairosg03@gmail.com>
2024-01-26 11:48:44 +0100

    a
    a

--- examen.php
+++ examen.php
@@ -58,15 +58,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $nombreArchivo = $_FILES["foto"]["name"];
         $extension = pathinfo($nombreArchivo, PATHINFO_EXTENSION);
 
-        $directorioDestino = $_SERVER['HOME'] . "/Descargas/";
-        
-        $rutaCompleta = $directorioDestino . $nombreArchivo;
-
-        move_uploaded_file($_FILES["foto"]["tmp_name"], $rutaCompleta);
-
-        echo "<p>Imagen subida:</p>";
-        echo "<img src='$rutaCompleta' alt='Imagen Subida'>";
+        echo "<p>La extensión de la foto es: $extension</p>";
     } else {
         echo "<p>No has seleccionado ninguna foto, o no ha cargado correctamente</p>";
     }
+}
 ?>
