77115e2b2d6c8035e22e8e69a49fb5f5e82e205e
Jairosz <Jairosg03@gmail.com>
2024-02-19 21:20:24 +0100

    fg
    fgf

---
+++ Stuff/test.js
@@ -0,0 +1,12 @@
+function abrirCerrarVentana() {
+  // Abre la ventana
+  let ventana = window.open("", "Ventana1", "width=150,height=150");
+
+  // Espera 2 segundos antes de cerrar la ventana
+//   setTimeout(function() {
+//     ventana.close();
+//   }, 2000);
+
+}
+
+
